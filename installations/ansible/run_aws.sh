d447527b6f0d65ddf60684c15ccd1e9fffde0400
oonisim <oonisim@gmail.colm>
2017-12-23 17:47:03 +1100

    Refactoring - use K8S_ADMIN instead of root for K8S operations. - Added gustbook example.

--- installations/ansible/run_aws.sh
+++ installations/ansible/run_aws.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+souce ~/.ssh/env_aws_keys.sh
+
 ENVIRONMENT=aws
 REMOTE_USER=centos
+HOSTFILE_DIR=/Users/maonishi/home/repositories/git/Kubernetes/installations/ansible/deploy/02_os/roles/hosts/files
 
 cd $(dirname $0)
-#aws/ec2/creation/scripts/main.sh master
-#aws/ec2/creation/scripts/main.sh worker
+#./aws/ec2/creation/scripts/main.sh ${ENVIRONMENT} master
+#./aws/ec2/creation/scripts/main.sh ${ENVIRONMENT} worker
+./aws/ec2/operations/scripts/generate_hosts.sh ${ENVIRONMENT} ${HOSTFILE_DIR}
+
+echo "update the env"
+read GO
+
 for module in $(find ./deploy -type d -maxdepth 1 -mindepth 1)
 do
     ${module}/scripts/main.sh ${ENVIRONMENT} ${REMOTE_USER}
